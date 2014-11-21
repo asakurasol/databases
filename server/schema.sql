a92d9c6fe6a8ddac42260ef1838bc1363107a983
asakurasol <mamamia1734@gmail.com>
2014-11-21 15:37:43 -0800

    updated client files

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,23 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` varchar(255),
+  `user` varchar(255),
+  `createdAt` varchar(255),
+  PRIMARY KEY (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
+INSERT INTO `messages` (text, user, createdAt)
+VALUES ('hello', 'I', 'now');
 
 
 /*  Execute this file from the command line by typing:
